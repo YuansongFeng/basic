cb16c6467550280d1c4caf8f8fcc7707b3da6d0c
Yuansong Feng <yf57@duke.edu>
2019-07-27 09:26:16 -0400

    implement and brief test resnet

--- models/resnet.py
+++ models/resnet.py
@@ -3,10 +3,50 @@
 import torch
 import torch.nn as nn
 
+class BasicBlock(nn.Module):
+    # block with 2 conv layers: 3x3xc_out[0] -> 3x3xc_out[1]
+    # surounded by residual connection 
+    def __init__(self, c_in, c_outs, stride=1):
+        super(BasicBlock, self).__init__()
+        assert len(c_outs) == 2, 'BasicBlock initialization error'
+
+        self.conv0 = nn.Conv2d(c_in, c_outs[0], 3, padding=1, stride=stride, bias=False)
+        self.bn0 = nn.BatchNorm2d(c_outs[0])
+        self.conv1 = nn.Conv2d(c_outs[0], c_outs[1], 3, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(c_outs[1])
+        # project identity to have the same dimension as the output 
+        self.conv_identity = nn.Conv2d(c_in, c_outs[1], 1, stride=stride)
+        self.bn_identity = nn.BatchNorm2d(c_outs[1])
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # x: B x C x W x H 
+        # B x c_outs[0] x W x H
+        out = self.conv0(x)
+        out = self.bn0(out)
+        out = self.relu(out)
+
+        # B x c_outs[1] x W x H
+        out = self.conv1(out)
+        out = self.bn1(out)
+
+        # B x c_outs[1] x W x H
+        identity = self.conv_identity(x)
+        identity = self.bn_identity(identity)
+
+        # residual connection
+        out = out + identity
+        out = self.relu(out)
+
+        return out
+
 class BottleneckBlock(nn.Module):
     # block with 3 conv layers: 1x1xc_out[0] -> 3x3xc_out[1] -> 1x1xc_out[2]
     # surounded by residual connection 
     def __init__(self, c_in, c_outs, stride=1):
+        super(BottleneckBlock, self).__init__()
+        assert len(c_outs) == 3, 'BottleneckBlock initialization error'
+
         self.conv0 = nn.Conv2d(c_in, c_outs[0], 1, stride=stride, bias=False)
         self.bn0 = nn.BatchNorm2d(c_outs[0])
         self.conv1 = nn.Conv2d(c_outs[0], c_outs[1], 3, padding=1, bias=False)
@@ -46,37 +86,38 @@ class BottleneckBlock(nn.Module):
 
 class ResNet(nn.Module):
     # type: 'resnet18', 'resnet50'
-    def __init__(self, type, num_classes):
+    def __init__(self, resnet_type, num_classes):
+        super(ResNet, self).__init__()
+        
         self.conv0 = nn.Conv2d(3, 64, 7, stride=2, padding=3, bias=False)
         self.max_pool = nn.MaxPool2d(3, stride=2, padding=1)
         # populate all residual layers
-        if self.type == 'resnet18':
-            self.res_layers = [ 
-                self._make_layer(BasicBlock, 64, [64, 64], 2),
+        if resnet_type == 'resnet18':
+            self.res_layers = nn.Sequential(
+                self._make_layer(BasicBlock, 64, [64, 64], 2, res_stride=1),
                 self._make_layer(BasicBlock, 64, [128, 128], 2),
                 self._make_layer(BasicBlock, 128, [256, 256], 2),
                 self._make_layer(BasicBlock, 256, [512, 512], 2)
-            ]
+            )
             self.projection = nn.Linear(512, num_classes)
-        elif self.type == 'resnet50':
-            self.res_layers = [
-                self._make_layer(BottleneckBlock, 64, [64, 64, 256], 3)
-                self._make_layer(BottleneckBlock, 256, [128, 128, 512], 4)
-                self._make_layer(BottleneckBlock, 512, [256, 256, 1024], 6)
+        elif resnet_type == 'resnet50':
+            self.res_layers = nn.Sequential(
+                self._make_layer(BottleneckBlock, 64, [64, 64, 256], 3, res_stride=1),
+                self._make_layer(BottleneckBlock, 256, [128, 128, 512], 4),
+                self._make_layer(BottleneckBlock, 512, [256, 256, 1024], 6),
                 self._make_layer(BottleneckBlock, 1024, [512, 512, 2048], 3)
-            ]
+            )
             self.projection = nn.Linear(2048, num_classes)
         
         self.avg_pool = nn.AvgPool2d(7)
-        self.softmax = nn.Softmax(dim=1)
     
-    def _make_layer(self, BlockType, channels_in, channels_out, num_layer):
+    def _make_layer(self, BlockType, channel_in, channels_out, num_layer, res_stride=2):
         layers = []
-        # first layer has a stride of 2
-        layers.append(BlockType(channels_in, channels_out, stride=2))
-        # rest layers have a stride of 1
+        # first block has a stride of 2 for the residual projection
+        layers.append(BlockType(channel_in, channels_out, stride=res_stride))
+        # rest blocks have a stride of 1
         for i in range(num_layer-1):
-            layers.append(BlockType(channels_in, channels_out))
+            layers.append(BlockType(channels_out[-1], channels_out))
 
         return nn.Sequential(*layers)
 
@@ -86,7 +127,12 @@ class ResNet(nn.Module):
         out = self.max_pool(out)
         for layer in self.res_layers:
             out = layer(out)
+        
         out = self.avg_pool(out)
-        out = self.softmax(out)
+        import pdb; pdb.set_trace()
+        # flatten W and H dimensions
+        out = out.flatten(start_dim=1)
+        import pdb; pdb.set_trace()
+        out = self.projection(out)
         
         return out
---
+++ unittests/resnet/train.py
@@ -0,0 +1,147 @@
+# Tests the implementation of custom resnet by 
+# training on Places365 dataset(mini). 
+# Check loss and accuracy curves, as well as final accuracy 
+# to get the sense of the performance of the model 
+import os
+import torch
+import torch.nn as nn
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+import torch.optim as optim
+from torchvision.datasets import ImageFolder
+# import torchvision.models as models
+from torch.utils.data import DataLoader
+import torchnet.meter as meter
+
+from models.resnet import ResNet
+
+def main():
+    # parameters
+    data_dir = '/data/feng/places365_mini'
+    checkpoint_dir = 'unittests/resnet/checkpoints'
+    learning_rate = 1e-3
+    weight_decay = 0.0
+    batch_size = 64
+    num_epochs = 50
+
+    # load model 
+    model = ResNet('resnet18', num_classes=10)
+    # model = models.resnet18(pretrained=False, num_classes=10)
+    model.cuda()
+    # import pdb; pdb.set_trace()
+
+    # define data transform and data loader 
+    train_loader = DataLoader(datasets.ImageFolder(
+        os.path.join(data_dir, 'train'), 
+        transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomResizedCrop(224, scale=(0.25, 1.0)), 
+            transforms.ToTensor(),
+            # expects a tensor
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ])), 
+        batch_size=batch_size, 
+        shuffle=True, 
+        # data go from SSD -> CPU-RAM -> GPU
+        # speed up I/O between CPU and GPU, but takes page-blocked memory on CPU
+        pin_memory=True,
+        # more num_workers consume more memory but speed up the I/O 
+        num_workers=4 
+    )
+    val_loader = DataLoader(datasets.ImageFolder(
+        os.path.join(data_dir, 'val'), 
+        transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ])),batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=4
+    )
+
+    criterion = nn.CrossEntropyLoss().cuda()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    
+    # training loop 
+    train_acc_hist = []
+    train_loss_hist = []
+    val_acc_hist = []
+    val_loss_hist = []
+    best_acc = 0.0
+    for epoch in range(num_epochs):
+        loss, acc = train(model, train_loader, criterion, optimizer)
+        train_acc_hist.append(acc)
+        train_loss_hist.append(loss)
+
+        loss, acc = validate(model, val_loader, criterion)
+        val_acc_hist.append(acc)
+        val_loss_hist.append(loss)
+
+        if acc > best_acc:
+            save_path = os.path.join(checkpoint_dir, 'best_acc.pth.tar')
+            torch.save(model.state_dict(), save_path)
+            best_acc = acc
+            print('model with accuracy %f saved to path %s' % (acc, save_path))
+        
+        print('****** epoch: %i val loss: %f val acc: %f best_acc: %f ******' % (epoch, loss, acc, best_acc))
+
+        # upload loss and acc curves to visdom
+        # viz(train_acc_hist, train_loss_hist, val_acc_hist, val_loss_hist)
+
+def train(model, dataloader, criterion, optimizer):
+    acc_meter = meter.AverageValueMeter()
+    loss_meter = meter.AverageValueMeter()
+
+    model.train()
+
+    for batch_idx, (inputs, targets) in enumerate(dataloader):
+        inputs = inputs.cuda()
+        targets = targets.cuda()
+        # outputs did NOT go thru softmax. per-class values don't sum to one
+        outputs = model(inputs)
+        preds = outputs.argmax(dim=1)
+        # check that the size matches
+        assert torch.equal(torch.tensor(preds.size()), torch.tensor(targets.size())), 'prediction and target size mismatch'
+
+        loss = criterion(outputs, targets)
+        # calculate and store partial derivatives
+        loss.backward()
+        # update all parameters based on partial derivatives
+        optimizer.step()
+
+        acc = calculate_acc(preds, targets)
+        acc_meter.add(acc)
+        loss_meter.add(loss.item())
+
+        if batch_idx % 100 == 0:
+            print('batch: %i loss: %f acc: %f' % (batch_idx, loss_meter.mean, acc_meter.mean))
+    return loss_meter.mean, acc_meter.mean
+
+    
+def validate(model, dataloader, criterion):
+    acc_meter = meter.AverageValueMeter()
+    loss_meter = meter.AverageValueMeter()
+
+    model.eval()
+
+    for batch_idx, (inputs, targets) in enumerate(dataloader):
+        inputs = inputs.cuda()
+        targets = targets.cuda()
+        outputs = model(inputs)
+        preds = outputs.argmax(dim=1)
+        # check that the size matches
+        assert torch.equal(torch.tensor(preds.size()), torch.tensor(targets.size())), 'prediction and target size mismatch'
+
+        loss = criterion(outputs, targets)
+        acc = calculate_acc(preds, targets)
+        acc_meter.add(acc)
+        loss_meter.add(loss.item())
+    return loss_meter.mean, acc_meter.mean
+
+# predictions and targets are both torch tensors 
+def calculate_acc(predictions, targets):
+    return (predictions == targets).sum().item() / predictions.size(0)
+    
+if __name__ == '__main__':
+    main()
